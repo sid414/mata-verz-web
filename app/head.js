017b34f5dc52ecaaceeb9ccc92423ccc1d8e467c
sid414 <siddharth182003@gmail.com>
2023-07-08 20:01:16 +0530

    commit

---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>Metaverz</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="favicon2.ico" />
+  </>
+);
+
+export default Head;
---
+++ constants/index.js
@@ -0,0 +1,93 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-14.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-08.png',
+    title: 'Crazy sky',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-13.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Embrace the chaos, embrace the magic. Enter the metaverz and rewrite your own story.',
+  'Step into the portal and unlock a universe where reality takes a vacation',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'We have a latest update with a new world for you to try. Venture beyond the matrix where the line between the real and virtual blurs into pure exhilaration.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your eyes are narrow, making the world more realistic than ever.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverz makes Elon musk ketar-ketir',
+    subtitle:
+    'A mix of excitement and nervousness, an entrepreneur\'s exhilarating sensation. The possibilities of the metaverse fill his mind with wonder and thrill. A transformative leap into the digital realm, where innovation and imagination fulfill.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'In the metaverse, thriving requires an adventurous spirit and an insatiable curiosity to unveil hidden gems. Collaboration with fellow explorers amplifies the metaverse experience, while adaptability and a commitment to online safety ensure a secure journey. Embracing innovation, maintaining a connection to the real world, and staying informed about metaverse trends foster continuous growth in this ever-evolving digital landscape.',
+  },
+  {
+    imgUrl: '/planet-03.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'With a single platform, the entire world becomes your virtual playground, ready to be explored. From iconic landmarks to remote destinations, the possibilities are limitless. Immerse yourself in immersive experiences, connecting with cultures, and expanding your horizons without leaving your doorstep. Discover the wonders of our planet through the power of technology, and let your curiosity take you on a journey of virtual exploration like never before.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link: 'https://twitter.com/i/flow/login?redirect_after_login=%2Fexplore'
+    
+
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/'
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/'
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    link: 'https://www.facebook.com/'
+  },
+];
